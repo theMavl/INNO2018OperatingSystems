07c92bda15444afcbeb10fde2025f85508db4904
Mavl <bmcmavl@gmail.com>
2018-11-08 21:26:48 +0300

    Ex1 fix

--- week12/ex1.c
+++ week12/ex1.c
@@ -6,13 +6,15 @@
 
 int main() {
     int devrand = open("/dev/random", O_RDONLY);
+    int out = open("ex1.txt", O_WRONLY | O_CREAT | O_TRUNC);
     if (devrand < 0) {
         return -1;
     } else {
         char *randData = malloc(sizeof(char) * 20);
         if (read(devrand, randData, 20) < 0)
             return -1;
-        printf("%s", randData);
+        write(out, randData, sizeof(char) * 20);
+        close(out);
     }
     return 0;
 }
