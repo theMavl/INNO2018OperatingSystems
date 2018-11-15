7c00a6b856aff8d8cdc4ab182da78004f2662e48
Mavl <bmcmavl@gmail.com>
2018-11-15 21:51:57 +0300

    Lab 13 fixes

--- week13/ex1.c
+++ week13/ex1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <tkPort.h>
 
+// Can't be bothered dealing with argv, so put dimension settings in compilation flags
+// E.g.: gcc ex1.c -o ex1 -DRES_N=4 -DPROC_N=6 -DLINE_LEN=128
 #ifndef RES_N
 #define RES_N 3
 #endif
@@ -15,6 +17,7 @@
 
 
 int main(int argc, char *argv[]) {
+    // For exercise it is not necessary to store all resources separately, just sum is enough
     int E = 0;
     int A = 0;
     int C[PROC_N] = {0};
@@ -23,6 +26,7 @@ int main(int argc, char *argv[]) {
     BOOL finished[PROC_N] = {False};
     char *file_path;
 
+    // Ability to read some other file name
     if (argc > 1)
         file_path = argv[1];
     else
@@ -36,13 +40,23 @@ int main(int argc, char *argv[]) {
 
     char buffer[LINE_LEN];
     char *ptr;
+
+    /*
+     * All values are read by blocks separately:
+     *
+     * BLOCK 0 - E (existing resources)
+     * BLOCK 1 - A (currently available processes)
+     * BLOCK 2 - C (current allocation)
+     * BLOCK 3 - R (requests)
+     */
     for (int block = 0; block < 4; block++) {
         for (int line = 0; line < PROC_N; line++) {
             ptr = fgets(buffer, LINE_LEN, input);
-
+            // Ignore empty lines
             while (ptr == NULL || strlen(buffer) <= 1)
                 fgets(buffer, LINE_LEN, input);
 
+            // Convert string to integers iteratively
             ptr = buffer;
             for (int row = 0; row < RES_N; row++) {
                 switch (block) {
@@ -60,11 +74,13 @@ int main(int argc, char *argv[]) {
                         break;
                 }
             }
+            // BLOCKS 0 and 1 have just one line, move to the next BLOCK
             if (block < 2)
                 break;
         }
     }
 
+    // Finding deadlocks
     BOOL found = True;
     while (found) {
         found = False;
